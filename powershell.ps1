ee8ecd0e9e9cae7117062d6bdefcfafaee9719f3
Happy Kharoud <44136380+HappyKharoud@users.noreply.github.com>
2025-08-18 10:26:36 -0400

    Update powershell.ps1

--- powershell.ps1
+++ powershell.ps1
@@ -15,6 +15,6 @@ $body = @{
 } | ConvertTo-Json -Depth 3
 
 Invoke-RestMethod -Method Post `
-    -Uri "https://prod-182.westus.logic.azure.com:443/workflows/ad1af5bfc93c49ff936414191686ff43/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=2P3pqVMErgeEF17ZNmvYa6MPgqamOMWroVPTRuk3bv0" `
+    -Uri "https://prod-128.westus.logic.azure.com:443/workflows/51187f1a876644878b940e83325a1482/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=0Z6TSfnkiRylRS9aFBJFBPoC0FDMyNqfMXkHE0qajwg" `
     -Body $body `
     -ContentType "application/json"
