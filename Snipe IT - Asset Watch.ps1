05d50c689076e706fd48a1f538d36267961f5e6d
Happy Kharoud <44136380+HappyKharoud@users.noreply.github.com>
2025-09-29 16:06:50 -0400

    Add files via upload

---
+++ Snipe IT - Asset Watch.ps1
@@ -0,0 +1,56 @@
+﻿param(
+    [string]$hostname,
+    [string]$username,
+    [string]$serial,
+    [string]$snipeDomain,
+    [string]$apiToken
+)
+
+$headers = @{
+    "Authorization" = $apiToken
+    "Accept" = "application/json"
+    "ContentType" = "application/json"
+}
+
+# Function to assign assets to users
+function assignTo() { 
+    $assetID = $asset.id
+    $postBody = @{
+        "checkout_to_type" = "user"
+        "assigned_user" = $snipeUserID
+        "status_id" = 2
+        "note" = "[AD Script] Asset $($asset.name) assigned to $($username). Serial Number: $($serial). Reason: $($username) signed in on an un-anssigned device."
+
+    }
+    $response = Invoke-RestMethod -Method Post -Uri "$snipeDomain/api/v1/hardware/$($assetID)/checkout" -Body $postBody -Headers $headers
+}
+
+# Function to get User ID from username
+function getUserID() {
+    try {
+        $response = Invoke-RestMethod -Method GET -Uri "$snipeDomain/api/v1/users?username=$username" -Headers $headers
+        return $response.rows.id
+    }
+    catch {
+        Write-Error "Failed to retrieve user ID: $($_.Exception.Message)"
+        Write-Error "Raw response: $($_.ErrorDetails.Message)"
+    }
+}
+
+$response = Invoke-RestMethod -Method GET -Uri "$snipeDomain/api/v1/hardware/byserial/$serial" -Headers $headers
+
+if ($response.status) {
+    Write-Warning "Asset does not exist"
+    exit
+}
+
+$asset = $response.rows[0]
+
+if ($asset.assigned_to) {
+    exit
+} else {
+    Write-Host "Assigned To: [Not assigned]"
+    $snipeUserID = getUserID
+    Write-Host "=================== Snipe ID:  $($snipeUserID) ==============="
+    assignTo
+}
